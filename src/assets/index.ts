ffc9707c5bab0a8d95b1f0dbb43ab65f8706c70a
rubayetseason <rubayetislamseason5@gmail.com>
2025-06-30 19:41:22 +0600

    font configuration

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+import logo_white from "@/assets/logos/logo_white.png";
+import logo_black from "@/assets/logos/logo_black.png";
+
+export const mindInkAssets = {
+  logos: {
+    logo_white,
+    logo_black,
+  },
+  images: {},
+};
---
+++ src/components/shared/layouts/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <div>
+<h1 className="text-3xl">Everyone has the right to freedom</h1>         
+        </div>
+    );
+};
+
+export default Navbar;
