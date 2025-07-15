6474ca7e0d3565efa112835f95180508899fff7e
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-16 04:04:22 +0600

    [FIX] --> vercel deploy error fix

--- src/app/(profile)/feed/[postId]/page.tsx
+++ src/app/(profile)/feed/[postId]/page.tsx
@@ -1,13 +1,18 @@
 import CommentSection from "@/components/shared/posts/CommentSection";
 import PostContent from "@/components/shared/posts/PostContent";
+import { type Metadata } from "next";
 
-interface PostPageProps {
-  params: {
-    postId: string;
-  };
+export const metadata: Metadata = {
+  title: "Post Details",
+  description: "View the full post details",
+};
+
+interface Props {
+  params: { postId: string };
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
-const SinglePost = ({ params }: PostPageProps) => {
+const SinglePost = ({ params }: Props) => {
   return (
     <div className="pb-10">
       <PostContent postId={params.postId} />
