d34cf67c4e14449b3792dba1525556252ac64893
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-16 03:55:36 +0600

    [FEAT] --> single blog post page with comment added

--- src/app/(profile)/feed/[postId]/page.tsx
+++ src/app/(profile)/feed/[postId]/page.tsx
@@ -1,20 +1,14 @@
-import CommentSection from "@/components/shared/posts/CommentSection";
-import PostContent from "@/components/shared/posts/PostContent";
-import { FC } from "react";
-
 interface PostPageProps {
   params: {
     postId: string;
   };
 }
 
-const SinglePost: FC<PostPageProps> = ({ params }) => {
-  console.log(params?.postId);
-
+const SinglePost = ({ params }: PostPageProps) => {
   return (
     <div className="pb-10">
-      <PostContent></PostContent>
-      <CommentSection></CommentSection>
+      <PostContent postId={params.postId} />
+      <CommentSection postId={params.postId} />
     </div>
   );
 };
